d62399cb614fac75fbfa03c8f9d30fa59604a90a
Merdan-Mahmudow <softprank@gmail.com>
2025-11-05 20:07:05 +0300

    modified:   src/routes/welcome.tsx

--- src/routes/welcome.tsx
+++ src/routes/welcome.tsx
@@ -1,6 +1,5 @@
 import { createFileRoute, useNavigate } from '@tanstack/react-router'
 import { WelcomeStories } from '../components/Stories'
-import logo from '../assets/img/logo.png'
 
 export const Route = createFileRoute('/welcome')({
   component: WelcomeScreen,
@@ -12,7 +11,7 @@ function WelcomeScreen() {
   return (
     <WelcomeStories
       stories={[
-        { id: 1, image: logo, text: 'Добро пожаловать в Trekopes' },
+        { id: 1, text: 'Добро пожаловать в Trekopes' },
         { id: 2, text: 'Создавайте треки и делитесь идеями мгновенно' },
         { id: 3, text: 'Готовы начать?' },
       ]}
