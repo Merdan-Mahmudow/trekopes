1762e1c8d46f7cc6cd7500cdf9d84429327b60fc
Merdan-Mahmudow <softprank@gmail.com>
2025-11-05 20:16:30 +0300

    renamed:    src/assets/img/logo-h.PNG -> public/logo-h.PNG 	renamed:    src/assets/img/logo.PNG -> public/logo.PNG 	renamed:    src/assets/img/pay_img.PNG -> public/pay_img.PNG 	modified:   src/components/Screens/ProPay/index.tsx 	new file:   src/global.d.ts

--- src/components/Screens/ProPay/index.tsx
+++ src/components/Screens/ProPay/index.tsx
@@ -1,7 +1,6 @@
 import { VStack, Text, Image, Grid } from "@chakra-ui/react"
 import { COLOR } from "../../ui/colors"
 import { BrandButton, GrayButton } from "../../ui/button"
-import logo from "../../../assets/img/pay_img.png"
 
 type ProPayScreenProps = {
     onBack?: () => void
@@ -15,7 +14,7 @@ export const ProPayScreen = ({ onBack, onPay }: ProPayScreenProps) => {
             <VStack w={"70vw"} textAlign={"center"} >
             <Text color={COLOR.kit.white}></Text>
             <Text color={COLOR.kit.orangeWhite}>Данные сохранены, пёс в студии🐾<br /> Осталось оплатить PRO-тариф — и трек пойдёт в работу 🎶</Text>
-            <Image src={logo} alt="logo" w="280px" h="280px" objectFit={"contain"} bg={"transparent"} />
+            <Image src={"/pay_img.png"} alt="logo" w="280px" h="280px" objectFit={"contain"} bg={"transparent"} />
             <Grid templateRows="1fr 1fr" gap={3} w="full" pt={4}>
                 <BrandButton w="full" onClick={onPay}>💳 Оплатить PRO</BrandButton>
                 <GrayButton w="full" onClick={onBack}>Назад</GrayButton>
